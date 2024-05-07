21915ae312263858d01a63050145d5b69d87a4c3
Satheeshmobiz <satheesh@mobiztechnologies.com>
2024-05-07 21:53:08 +0530

    ver67

--- fyersapi/routing.py
+++ fyersapi/routing.py
@@ -1,14 +1,14 @@
 # routing.py
 
-from django.urls import path
+from django.urls import re_path
 from .consumers import  FyersPositionDataConsumer, FyersIndexDataConsumer
 from django.urls import path
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 ws_urlpatterns = [
-    path('ws/fyersindexdata/<str:last_keyword>/', FyersIndexDataConsumer.as_asgi()),
-    path('ws/fyerspositionsdata/', FyersPositionDataConsumer.as_asgi()),
+    re_path('ws/fyersindexdata/<str:last_keyword>/', FyersIndexDataConsumer.as_asgi()),
+    re_path('ws/fyerspositionsdata/', FyersPositionDataConsumer.as_asgi()),
 
 
     
